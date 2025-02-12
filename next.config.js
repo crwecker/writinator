1dba44253795b8e1e2834eab55713d70fab00280
Carl Wecker <crwecker@gmail.com>
2025-02-11 21:50:24 -0600

    Setup github actions to build and push to gh-pages branch

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['jsx', 'js', 'tsx', 'ts'],
   images: {
+    unoptimized: true,
     domains: ['res.cloudinary.com'],
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/writinator' : '/',
-  basePath: process.env.NODE_ENV === 'production' ? '/writinator' : '',
+  }
 }
 
 export default nextConfig
